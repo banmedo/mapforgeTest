410c37c9ed3b022ee4355c7ab71569efadefd3b6
banmedo <ban.medo@live.com>
2014-03-19 09:28:22 +0545

    first commit

---
+++ src/org/ban/mapforgetest/MainActivity.java
@@ -0,0 +1,29 @@
+package org.ban.mapforgetest;
+
+import org.mapsforge.android.maps.MapView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		MapView mapView = (MapView) findViewById(R.id.map);
+		mapView.setClickable(true);
+		mapView.setBuiltInZoomControls(true);
+		
+		this.setContentView(mapView);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
